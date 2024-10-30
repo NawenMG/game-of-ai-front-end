9efbe8cd3477b6b2d4de1daf212f32db600d11af
Mario <GranatoM@outlook.it>
2024-10-30 10:39:43 +0100

    loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorsComponent } from './errors/errors.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ErrorsComponent],
+  imports: [RouterOutlet, HomeComponent, ErrorsComponent, LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
